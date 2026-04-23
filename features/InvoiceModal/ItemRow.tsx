ef4f6afcb5c75dd0d2021871dbbfba416333b1a5
Abdulrahman Abdulrahim <a.raheeem0001@gmail.com>
2026-04-24 00:27:34 +0100

    refactor: Moved Trashcan component to seperate file

--- features/InvoiceModal/ItemRow.tsx
+++ features/InvoiceModal/ItemRow.tsx
@@ -1,4 +1,5 @@
 import { FormField, inputBase, inputError, cx } from './FormField';
+import { TrashIcon } from '@/components/TrashCan';
 
 export interface InvoiceItem {
   name: string;
@@ -105,16 +106,3 @@ export function ItemRow({ item, errors, showErrors, onRemove, onUpdate }: ItemRo
   );
 }
 
-
-function TrashIcon() {
-  return (
-    <svg width="13" height="16" viewBox="0 0 13 16" fill="none">
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M8.47 0L9.36.9H13v1.8H0V.9h3.64L4.53 0h3.94zM1 14.1C1 15.14 1.9 16 3 16h7c1.1 0 2-.86 2-1.9V3.6H1v10.5z"
-        fill="currentColor"
-      />
-    </svg>
-  );
-}
